45632295ba6e5ab2575d72e86cb5b51bc70b7cbf
Pratik667 <44764336+Pratik667@users.noreply.github.com>
2021-04-29 14:09:18 +0530

    fd

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-    //Navbar Nav-items from top bar added here
-    var find_dealer = "<li class='nav-item'><a class='nav-link' href='#'>Find Dealer</a></li>"
-      $(find_dealer).insertAfter(".navbar-nav:first-child .nav-item:last-child");
-    var call_assist = "<li class='nav-item'><a class='nav-link' href='#'>Call <span class='text-blue'>1800 209 8282</span> For Any Assistance</a></li>"
-    $(call_assist).insertAfter(".navbar-nav:first-child .nav-item:last-child");
+
+    
+    if ($(window).width() < 960) {
+        //Navbar Nav-items from top bar added here in vw 960px
+        var find_dealer = "<li class='nav-item'><a class='nav-link' href='#'>Find Dealer</a></li>"
+        $(find_dealer).insertAfter(".navbar-nav:first-child .nav-item:last-child");
+        var call_assist = "<li class='nav-item'><a class='nav-link' href='#'>Call <span class='text-blue'>1800 209 8282</span> For Any Assistance</a></li>"
+        $(call_assist).insertAfter(".navbar-nav:first-child .nav-item:last-child");
+     }
+
+   
 });
